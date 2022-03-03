5e883b7b8b03d7d172675bd86267cfb0938a8710
gary <gary.white@lodgit.com>
2022-03-03 12:57:14 +0100

    code +

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function tripleTrouble(one, two, three){
     //Solution
+    return one+two+three;
 }
 
-console.log(tripleTrouble("aaa", "bbb", "ccc")); // return "aaabbbccc"
+console.log(tripleTrouble("aaa", "bbb", "ccc")); // return "abcabcabc"
+console.log(tripleTrouble("aaaaaa","bbbbbb","cccccc")); // return "abcabcabcabcabcabc"
+console.log(tripleTrouble("burn", "reds", "roll")); // return "brrueordlnsl"
+console.log(tripleTrouble("Sea","urn","pms")); // return "Supermans"
+console.log(tripleTrouble("LLh","euo","xtr")); // return "LexLuthor"
